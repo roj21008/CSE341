67756b3c3379950a97f85f2bb0417b70e726801d
roj21008 <roj21008@byui.edu>
2023-06-06 22:54:13 -0500

    first commit(first assigment)

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+const awesomeFunction = (req,res,next)=>{
+    res.json('LUIS ROJAS HERNANDEZ');
+};
+
+const returnAnotherPerson = (req,res,next)=>{
+    res.json('Super awesome person');
+};
+
+
+module.exports = {awesomeFunction,returnAnotherPerson};
